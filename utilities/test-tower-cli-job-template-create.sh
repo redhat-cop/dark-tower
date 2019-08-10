d6a395c8236d8e4cb1a15ef77d44ea84a8d7978d
Tok <tony.g.kay@gmail.com>
2019-08-10 06:48:31 -0600

    Captured cli command to add vault inventory

---
+++ utilities/test-tower-cli-job-template-create.sh
@@ -0,0 +1,17 @@
+
+# 1st command works
+
+ tower-cli job_template associate_credential --job-template job-runner --credential babylon-tower-credential
+
+
+
+
+#tower-cli job_template create     \
+#--name=test_job_template          \
+#--project="babylon" \
+#--inventory="empty-inventory-default" \
+#--playbook=job-runner.yml         \
+#--tower-credential="babylon-tower-credential"  \
+#--extra-vars="a=5 b=3"           \ 
+#--extra-vars="@filename.yml"
+#--machine-credential=1            \
